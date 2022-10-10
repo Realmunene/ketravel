c417c4c73a5e2dc89bc59db116ced6b871d719a4
Realmunene <josep.mwangi@student.moringaschool.com>
2022-10-10 23:23:19 +0300

    Updates

---
+++ precache-manifest.b05b8562ecd9d0d36deb5ab08c07b2ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7977e4af954bc6c0551a",
+    "url": "/ketravel/static/css/main.ab8e6d2c.chunk.css"
+  },
+  {
+    "revision": "7977e4af954bc6c0551a",
+    "url": "/ketravel/static/js/main.7977e4af.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/ketravel/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "9d4a11d3efffa3456d7f",
+    "url": "/ketravel/static/js/2.9d4a11d3.chunk.js"
+  },
+  {
+    "revision": "a5567688e1edc179c598",
+    "url": "/ketravel/static/js/runtime~main.a5567688.js"
+  },
+  {
+    "revision": "1d6608ba5d3f7365672a6c1351c22574",
+    "url": "/ketravel/static/media/backg.1d6608ba.jpg"
+  },
+  {
+    "revision": "ad850d25c6dcc123b2f2b6b694eb51e9",
+    "url": "/ketravel/static/media/onea.ad850d25.jpg"
+  },
+  {
+    "revision": "c615f0e769af0b0c13f3b19faca0ff2d",
+    "url": "/ketravel/index.html"
+  }
+];
